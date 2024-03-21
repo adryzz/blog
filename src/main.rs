c4010ab6ed71d27a85f3ce5030483d441aca9adb
Lena <lena@nihil.gay>
2024-03-21 02:01:08 +0100

    update all dependencies

--- src/main.rs
+++ src/main.rs
@@ -27,12 +27,10 @@ async fn run() -> anyhow::Result<()> {
         .nest("/blog", axum_static::static_router("blog"))
         .nest("/badges", axum_static::static_router("badges"));
 
-    let listener = std::net::TcpListener::bind("0.0.0.0:3000")?;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     tracing::info!("Listening on {}...", listener.local_addr()?);
 
-    axum::Server::from_tcp(listener)?
-        .serve(app.into_make_service())
-        .await?;
+    axum::serve(listener, app).await?;
 
     Ok(())
 }
